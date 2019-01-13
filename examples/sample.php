a6bf3c5b8dc6d92d5786e5e17baeafcade94f129
Giovani Paseto <giovaniw2@gmail.com>
2019-01-13 11:15:14 -0200

    Travis/packagist

--- examples/sample.php
+++ examples/sample.php
@@ -3,12 +3,18 @@ ini_set('display_errors', 1);
 require '../vendor/autoload.php';
 
 $senig = new \App\Senig();
-
-$result = $senig->enviaXMLRet('../43190186933033000100570010001272871190127212-cte.xml', '1', '127287');
+$doc = '00000000000000';
+$result = $senig->enviaXMLRet('43171100000000000000570010001007561171170010-cte.xml', $doc, '127287');
 if ($result == false) {
     echo $senig->getErrors();
 } else {
-    if ($result->object->listaMensagem->dStatus !== 100) {
+//    echo '<pre>';
+//    print_r($result->object);
+    if ($result->object->listaMensagem->dStatus != 100) {
         echo $result->object->listaMensagem->dDetErr;
+    }else{
+        echo $result->object->dtRec;
+        echo $result->object->cProtocolo;
+        echo $result->object->listaMensagem->cStatus;
     }
 }
